aef77992f98e1f5cab57751317329226f3084b0f
sal-hir <hirushakisal@gmail.com>
2026-02-18 17:18:11 +0530

    Ui change user dropdown

--- easy-blogger/components/layout/Header.jsx
+++ easy-blogger/components/layout/Header.jsx
@@ -52,6 +52,11 @@ export default function Header({ onToggleSidebar }) { //onToggleSidebar is a fun
         </button>
 
         <Link href="/home" className="flex items-center gap-2">
+         <img
+              src="/images/easy-blogger-logo.png"
+              alt="Easy Blogger Logo"
+              className="h-12 w-auto"
+            />
           <h1 className="text-2xl font-bold text-[#1ABC9C] font-serif">
             Easy Blogger
           </h1>
@@ -98,13 +103,24 @@ export default function Header({ onToggleSidebar }) { //onToggleSidebar is a fun
           </button>
 
           {open && (
-            <div className="absolute right-0 mt-2 w-56 bg-white border rounded-xl shadow-xl py-2 animate-in fade-in zoom-in-95 duration-100">
-              <div className="px-4 py-2 border-none mb-1 text-sm">
+            <div className="absolute right-0 mt-2 w-56 bg-white border border-[#e5e7eb] rounded-xl shadow-xl py-2 animate-in fade-in zoom-in-95 duration-100">
+              <div className = "w-17 h-17 rounded-full border-0 overflow-hidden mx-auto">
+                <img src={user.avatar} alt="User" className="w-full h-full object-cover" />
+              </div>
+              <div className="px-4 py-2 border-none mb-1 text-sm   mb-0">
                 <p className="font-bold truncate">
-                  <Link href="./profile"> {user.name}</Link>
+                 
+                  <Link href="./profile">
+                   
+                    {user.name} 
+                     
+                  </Link>
+                 
                 </p>
                 <p className="text-gray-500 text-xs truncate">{user.email}</p>
+
               </div>
+              
 
               {/*icon help, message,membership from menulink function*/}
 
